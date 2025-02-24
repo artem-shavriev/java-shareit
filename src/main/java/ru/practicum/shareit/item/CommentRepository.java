8758227a4122a2a8ac84a7e5bdb89f317bf79b5f
Artem Shavriev <artem-shavriev@yandex.ru>
2025-02-24 16:12:56 +0300

    - Доработать стиль.

--- src/main/java/ru/practicum/shareit/item/CommentRepository.java
+++ src/main/java/ru/practicum/shareit/item/CommentRepository.java
@@ -3,7 +3,6 @@ package ru.practicum.shareit.item;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import ru.practicum.shareit.item.model.Comment;
-import ru.practicum.shareit.item.model.Item;
 
 import java.util.List;
 
@@ -11,5 +10,5 @@ public interface CommentRepository extends JpaRepository<Comment, Integer> {
     @Query("select c from Comment as c " +
             "join c.item as i " +
             "where i.id = ?1")
-    List<Comment> findAllComment(Integer ItemId);
+    List<Comment> findAllComment(Integer itemId);
 }
