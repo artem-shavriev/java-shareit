2b0cde2a06c476bb7a3c6de085d5f9b01e61ab87
Artem Shavriev <artem-shavriev@yandex.ru>
2025-02-24 16:09:29 +0300

    - Доработать стиль.

--- src/main/java/ru/practicum/shareit/booking/BookingMapper.java
+++ src/main/java/ru/practicum/shareit/booking/BookingMapper.java
@@ -15,7 +15,6 @@ import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
 import java.util.List;
 
 @Component
